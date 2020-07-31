2c13dd024f32cbac6564e2e70599ced963453a7b
dafei9522 <1047225269@qq.com>
2020-07-31 17:30:19 +0800

    first commit

--- src/components/headerTop/headertop.vue
+++ src/components/headerTop/headertop.vue
@@ -1,12 +1,16 @@
 
 <template>
 	<div class='header'>
-		<div class="title">
-			<slot name="title"></slot>
-		</div>
-		<div class="bt">
-			<slot name="bt"></slot>
-		</div>
+		<div class="title">{{title}}</div>
+			<div class="btborder">
+				<div @click="more">
+					<img :src="imgs.more" alt=""/>
+				</div>
+				<p></p>
+				<div @click="mao">
+					<img :src="imgs.mao" alt=""/>
+				</div>
+			</div>
 	</div>
 </template>
 
@@ -14,7 +18,10 @@
 //import x from ''
 export default {
 	name: 'headertop',
-	props: {},
+	props: {
+		title:String,
+		imgs:Object
+	},
 	data () {
 		return {
 		}
@@ -22,12 +29,22 @@ export default {
 	components: {},
 	watch: {},
 	computed: {},
-	created () {},
+	created () {
+		console.log(this.imgs)
+	},
 	mounted () {},
-	methods: {},
+	methods: {
+			more(){
+				console.log(2222)
+			},
+			mao(){
+				console.log(111)
+			}
+		}
 }
 </script>
 <style scoped lang="less">
+@import '../../style/index.less';
 .header{
 	width: 100%;
 	height: 44px;
@@ -36,9 +53,34 @@ export default {
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	background:@headerg;
+	color: @fontColorF;
 	.title{
 		font-size:18px;
 		font-weight:500;
 	}
+}
+.btborder{
+	width: 70px;
+	height: 23px;
+	border:@borderW;
+	border-radius: 50px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
+.btborder p {
+	border: 0.5px solid #ffffff;
+	height: 10px;
+	background-color: #fff;
+}
+.btborder div{
+	display: flex;
+	// justify-content: space-around;
+	align-items: center;
+}
+.btborder div img{
+	width: 20px;
+	height: 20px;
 }
 </style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,35 +1,57 @@
 <template>
   <div class="wrap">
-		<div class="header">
-			<headertop>
-				<div slot="title">首页</div>
-				<div slot="bt">
-					<div class="btborder"></div>
-				</div>
-			</headertop>
-		</div>
-      
+			<!-- 头部组件 -->
+			<headertop :title="title" :imgs='imgs'></headertop>
   </div>
 </template>
 <script>
 import headertop from '@/components/headerTop/headertop'
+import maoImg from '../../assets/img/home/mao.png'
+import moreImg from '../../assets/img/home/more.png'
 export default {
 		name:'Home',
 		components: {
 			headertop
+		},
+		data () {
+			return {
+				title:'首页',
+				imgs:{
+					more:moreImg,
+					mao:maoImg
+				}
+			}
+		},
+		methods: {
 		}
 }
 </script>
 <style scoped lang="less">
-@import '../../style/index.less';
-.header{
-	background:@headerg;
-	color: @fontColorF;
-}
-.btborder{
-	width: 70px;
-	height: 23px;
-	border:@borderW;
-	border-radius: 50px;
-}
+// .header{
+// 	background:@headerg;
+// 	color: @fontColorF;
+// }
+// .btborder{
+// 	width: 70px;
+// 	height: 23px;
+// 	border:@borderW;
+// 	border-radius: 50px;
+// 	display: flex;
+// 	justify-content: space-around;
+// 	align-items: center;
+// }
+// .btborder p {
+// 	border: 0.5px solid #ffffff;
+// 	height: 10px;
+// 	background-color: #fff;
+// }
+// .btborder div{
+// 	display: flex;
+// 	// justify-content: space-around;
+// 	align-items: center;
+// }
+// .btborder div img{
+// 	width: 20px;
+// 	height: 20px;
+// }
 </style>
